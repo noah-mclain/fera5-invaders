8e29cf10f807f0c1fb3697c66967dd607281de5b
omariko63 <120332938+omariko63@users.noreply.github.com>
2024-11-28 16:25:58 +0200

    First version of player and laser classes

--- src/laser.py
+++ src/laser.py
@@ -1,3 +1,34 @@
+import pygame
 # Laser class for shooting mechanics
-def __init__():
-    ...
+class Laser():
+    def __init__(self, x, y):
+        self.image = pygame.image.load("assets\images\bullet\a1.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = -3
+        self.is_fired = False
+        self.is_engaged = False
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def update(self, screen):
+        self.rect.y += self.speed
+        if self.rect.y > 0 or self.is_engaged:
+            self.die()
+
+
+    def fire(self):
+        self.is_fired = True
+
+    def die(self):
+        self.is_fired = False
+
+    def engage(self):
+        self.is_engaged = True
+        self.die()
+
+
+
+
--- src/player.py
+++ src/player.py
@@ -1,19 +1,51 @@
 # Player class managing player actions
 import pygame
+from laser import Laser
 
 class Player:
     def __int__(self, x,y):
         self.image = pygame.image.load("assets\images\ship.png")
-        self.rect = self.image.get_rect((x,y))
-        
+        self.rect = self.image.get_rect()
+        info = pygame.display.Info()
+        self.screen.width = info.current_w
+        self.screen.height = info.current_h
+        self.rect.x = self.screen.width /2
+        self.rect.y = self.screen.height - 15
+        self.speed = 0
+        self.max_speed = 5 
+        self.alive = True
+
 
     def shoot(self):
-        ...
+        laser = Laser(self.rect.x, self.rect.y)
+        laser.fire()
 
+    # player movement
     def move(self,change):
-        self.rect.x += change
-        return self.rect.x
+        self.speed += change
+            
+    def stop(self):
+        self.speed = 0
     
+
+    # drawing the player ship
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.x > self.screen.width - self.rect.width:
+            self.rect.x = self.screen.width - self.rect.width
+
+    def die(self):
+        self.image = pygame.image.load("assets\images\shipDie.png")
+        self.alive = False
+
+        
+       
+
 
     
 
