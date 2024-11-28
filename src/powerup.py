a29a89811311fec67b919b267fcd86c3aa683170
noah-mclain <nadam.30032415@gmail.com>
2024-11-28 18:01:59 +0200

    created sprites for each powerup

--- src/powerup.py
+++ src/powerup.py
@@ -1,1 +1,10 @@
 # Power-up class for bonuses
+import pygame
+import random
+
+class PowerUp:
+    def __init__(self, x, y):
+        self.image = pygame.image.load()
+    
+    def apply_effect(self):
+        pass
