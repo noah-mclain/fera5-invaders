4207fe86642ee84dc239ab9fbcc44669e7d7223e
omariko63 <120332938+omariko63@users.noreply.github.com>
2024-12-01 22:00:01 +0200

    Co-authored-by: Rola  <rolahadidy44@users.noreply.github.com> Co-authored-by: Nada Mohamed <noah-mclain@users.noreply.github.com>

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -3,6 +3,8 @@ from enemy import Chicken
 from game import Game
 from random import random
 import tensorflow as tf
+import gymnasium as gym
+
 
 
 class ai_env:
@@ -10,18 +12,44 @@ class ai_env:
         self.player = game.player
         self.enemies = game.enemies
         self.score = 0
-        self.actions=["right", "left", "shoot", "stop"]
+        self.actions=["right", "left", "shoot", "stop"]  
         
                
     def available_actions(self):
         available_actions = ["shoot", "stop"]
         if self.player.rect.x < self.game.screen_width:
-            available_actions.append["left"]
+            available_actions.append("left")
         if self.player.rect.x > 0:
-            available_actions.append["right"]
+            available_actions.append("right")
+    
+    def input_nodes(self):
+        player_nodes = 2
+        MAX_chicken_nodes = 40 * 2
+        MAX_egg_nodes = MAX_chicken_nodes
+        MAX_laser_nodes = 20
+        MAX_powerups_nodes = 6
+        total_nodes = player_nodes + MAX_chicken_nodes + MAX_egg_nodes + MAX_laser_nodes + MAX_powerups_nodes
+        return total_nodes
+
+
 
 class AI:
     def __init__(self, environment, alpha, epsilon):
         self.environment = environment
         self.alpha = alpha
         self.epsilon = epsilon
+    
+    def get_input_layer(self):
+        self.input_nodes = self.environment.input_nodes()
+        return self.input_nodes
+
+class DQNMODEL(tf.keras.models):
+    def __init__(self, input_size, num_actions):
+        super(DQNMODEL, self).__init__()
+        
+
+        
+    
+    
+
+    
